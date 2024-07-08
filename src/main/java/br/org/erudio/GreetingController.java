98f1830437e8e9534884ec86d7925681d2aca79e
Davi Gabriel <davig41441503@gmail.com>
2024-07-08 15:42:03 -0300

    first RestController

---
+++ src/main/java/br/org/erudio/GreetingController.java
@@ -0,0 +1,18 @@
+package br.org.erudio;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong(); //mockar um id
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name){
+        return new Greeting(counter.incrementAndGet(),String.format(template,name));
+    }
+}
