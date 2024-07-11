8b58fcc616400a7fff4b45ba6ee94ca496dbf4d2
Davi Gabriel <davig41441503@gmail.com>
2024-07-11 13:19:11 -0300

    refactoring controller and status code 204 for delete verb

--- 08_CreatingDataBaseAndJPA/src/main/java/br/org/davi/controllers/PersonController.java
+++ 08_CreatingDataBaseAndJPA/src/main/java/br/org/davi/controllers/PersonController.java
@@ -4,6 +4,7 @@ import br.org.davi.model.Person;
 import br.org.davi.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,32 +19,31 @@ public class PersonController{
 
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping(value = "/{id}",method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE/*indica que produz json*/)
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE/*indica que produz json*/)
     public Person findById(@PathVariable(value = "id") Long id) throws Exception{
         return service.findById(id);
     }
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE/*indica que produz json*/)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE/*indica que produz json*/)
     public List<Person> findAll(){
         return service.findAll();
     }
 
-    @RequestMapping(method = RequestMethod.POST,
-            produces = MediaType.APPLICATION_JSON_VALUE/*indica que produz json*/,
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE/*indica que produz json*/,
             consumes = MediaType.APPLICATION_JSON_VALUE/*indica que consome json*/)
     public Person create(@RequestBody Person person) throws Exception{
         return service.create(person);
     }
 
-    @RequestMapping(method = RequestMethod.PUT,
-            produces = MediaType.APPLICATION_JSON_VALUE/*indica que produz json*/,
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE/*indica que produz json*/,
             consumes = MediaType.APPLICATION_JSON_VALUE/*indica que consome json*/)
     public Person update(@RequestBody Person person) throws Exception{
         return service.update(person);
     }
 
-    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
-    public void delete(@PathVariable(value = "id") Long id){
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id){
         service.delete(id);
+        return ResponseEntity.noContent().build(); //retorna o status code 204 no content, ideal para delete
     }
 
 }
