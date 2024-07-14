8930f30b2db1b966c786c2b64ae0f2fbb9bacf4b
Davi Gabriel <davig41441503@gmail.com>
2024-07-14 11:08:37 -0300

    custom json serialization

--- 11_MigrationsWithFlyWay/src/main/java/br/org/davi/data/vo/v1/PersonVO.java
+++ 11_MigrationsWithFlyWay/src/main/java/br/org/davi/data/vo/v1/PersonVO.java
@@ -1,16 +1,23 @@
 package br.org.davi.data.vo.v1;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.io.Serializable;
 import java.util.Objects;
 
-
+@JsonPropertyOrder({"id","first_name","last_name","address","gender"})
 public class PersonVO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long id;
+    @JsonProperty("first_name")
     private String firstName;
+    @JsonProperty("last_name")
     private String lastName;
     private String address;
+    //@JsonIgnore
     private String gender;
 
     public PersonVO() {
