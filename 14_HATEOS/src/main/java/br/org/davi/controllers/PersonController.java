594972a7a03685c3f053eca2e6c1217cd9a7b7a9
Davi Gabriel <davig41441503@gmail.com>
2024-07-15 19:55:25 -0300

    HATEOS in CRUD

---
+++ 14_HATEOS/src/main/java/br/org/davi/controllers/PersonController.java
@@ -0,0 +1,49 @@
+package br.org.davi.controllers;
+
+import br.org.davi.data.vo.v1.PersonVO;
+import br.org.davi.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import br.org.davi.util.MediaType;
+
+import static br.org.davi.util.MediaType.*;
+
+@RestController
+@RequestMapping("/api/person/v1") //mapping a nivel de controller
+public class PersonController{
+
+    @Autowired //fará a instanciação do service em tempo de execução (private PersonService service = new PersonService())
+    private PersonService service;
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML}/*indica que produz json*/)
+    public PersonVO findById(@PathVariable(value = "id") Long id) throws Exception{
+        return service.findById(id);
+    }
+    @GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML})
+    public List<PersonVO> findAll(){
+        return service.findAll();
+    }
+
+    @PostMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML}, consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML}/*indica que consome json*/)
+    public PersonVO create(@RequestBody PersonVO person) throws Exception{
+        return service.create(person);
+    }
+
+    @PutMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML}, consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML}/*indica que consome json*/)
+    public PersonVO update(@RequestBody PersonVO person) throws Exception{
+        return service.update(person);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build(); //retorna o status code 204 no content, ideal para delete
+    }
+
+}
